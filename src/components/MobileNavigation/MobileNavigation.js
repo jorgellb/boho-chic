d5cae066f4851619331425f43bf7e8605c3f6364
Jorge Luis <jorgellb@gmail.com>
2026-01-28 23:20:02 +0100

    Update MobileNavigation.js

--- src/components/MobileNavigation/MobileNavigation.js
+++ src/components/MobileNavigation/MobileNavigation.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import { Link, navigate } from 'gatsby';
+import { Link } from 'gatsby';
 
 import Config from '../../config.json';
 import Icon from '../Icons/Icon';
-import { isAuth } from '../../helpers/general';
 
 //TO DO: refactor this to handle multiple nested links to avoid hardcoding 'depth'
 // have to restructure config.json
@@ -18,47 +17,10 @@ const MobileNavigation = (props) => {
   const [category, setCategory] = useState();
   const [depth, setDepth] = useState(0);
 
-  const handleLogout = () => {
-    window.localStorage.removeItem('key');
-    navigate('/');
-    close();
-  };
-
   return (
     <div className={styles.root}>
       <nav>
         <div className={styles.headerAuth}>
-          {depth === 0 && isAuth() === false && (
-            <div className={styles.authLinkContainer}>
-              <Link to={'/signup'}>Sign Up</Link>
-              <Link to={'/login'}>Login</Link>
-            </div>
-          )}
-
-          {depth === 0 && isAuth() === true && (
-            <div
-              className={styles.welcomeContainer}
-              role={'presentation'}
-              onClick={() => setDepth(-1)}
-            >
-              <span className={styles.welcomeMessage}>Welcome, John</span>
-              <Icon symbol={'caret'}></Icon>
-            </div>
-          )}
-
-          {depth === -1 && isAuth() === true && (
-            <div
-              className={styles.previousLinkContainer}
-              onClick={() => setDepth(0)}
-              role={'presentation'}
-            >
-              <div className={styles.previousIcon}>
-                <Icon symbol={'caret'}></Icon>
-              </div>
-              <span>my account</span>
-            </div>
-          )}
-
           {depth === 1 && (
             <div
               className={styles.previousLinkContainer}
@@ -110,12 +72,6 @@ const MobileNavigation = (props) => {
                   </Link>
                 );
               })}
-              <div className={styles.navFooter}>
-                <Link to={'/favorites'}>
-                  <Icon symbol={'heart'} />
-                  Favorites (0)
-                </Link>
-              </div>
             </div>
           )}
 
@@ -149,35 +105,6 @@ const MobileNavigation = (props) => {
                 </Link>
               );
             })}
-
-          {depth === -1 && (
-            <>
-              <div>
-                <Link to={'/account/orders/'} className={styles.mobileLink}>
-                  Orders
-                </Link>
-                <Link to={'/account/address/'} className={styles.mobileLink}>
-                  Addresses
-                </Link>
-                <Link to={'/account/settings/'} className={styles.mobileLink}>
-                  Settings
-                </Link>
-                <Link to={'/account/viewed/'} className={styles.mobileLink}>
-                  Recently Viewed
-                </Link>
-              </div>
-              <div className={styles.navFooter}>
-                <div
-                  className={styles.logoutContainer}
-                  role={'presentation'}
-                  onClick={handleLogout}
-                >
-                  <Icon symbol={'logout'} />
-                  <span>Sign out </span>
-                </div>
-              </div>
-            </>
-          )}
         </div>
       </nav>
     </div>
